a28cc845a902a36d04da2f8fc9de426d3de995a4
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-11-18 18:49:56 +0500

    181

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
     'mongodb+srv://husan:husan@shop.qbc0hcq.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then((client) => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
