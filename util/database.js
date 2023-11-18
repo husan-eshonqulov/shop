9bb17941a12348b8d06b14deddd0de654301003a
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-11-18 17:43:09 +0500

    180

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://husan:husan@shop.qbc0hcq.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
