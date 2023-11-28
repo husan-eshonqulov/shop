ae9d806f0bdbd9d3e0246d31722215baa9884d8b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-11-28 15:02:34 +0500

    194

--- models/user.js
+++ models/user.js
@@ -1,16 +1,24 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+const ObjectId = mongodb.ObjectId;
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users').inserOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users').findOne({ _id: new ObjectId(userId) });
+  }
+}
 
 module.exports = User;
