b742243df396440d968205f52d62974479065646
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-11-28 17:51:54 +0500

    199

--- controllers/shop.js
+++ controllers/shop.js
@@ -89,17 +89,12 @@ exports.postCart = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products,
-          });
-        })
-        .catch((err) => console.log(err));
+    .then((products) => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+      });
     })
     .catch((err) => console.log(err));
 };
