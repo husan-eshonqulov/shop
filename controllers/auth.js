2bb902d47144318c9fa5b6b1e18c00a8aa88c96c
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-12-18 16:25:48 +0500

    255

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -40,9 +42,12 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPass) => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPass,
         cart: { items: [] },
       });
       return user.save();
