860115fbd2a24ab2fd8f27cf5efecf57a88af96f
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-12-19 11:22:02 +0500

    268

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password');
+        req.flash('error', 'Invalid email');
         return res.redirect('/login');
       }
       bcrypt
@@ -36,6 +36,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid password');
           res.redirect('/login');
         })
         .catch((err) => {
@@ -47,9 +48,16 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message,
   });
 };
 
@@ -60,6 +68,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'Email exists already, please pick a different one');
         return res.redirect('/signup');
       }
       return bcrypt
